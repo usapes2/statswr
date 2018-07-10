7265b683a6fb416c18a364d38b2ecfec0d7ae34c
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-07-10 11:58:10 -0500

    Example 4.1 solution

--- First_script.R
+++ First_script.R
@@ -1,3 +1,18 @@
-#This is just a test
-a = 1 
-b = 2
+# A lot containing 7 components is sampled by a quality inspector; the lot contains
+# 4 good components and 3 defective components. A sample of 3 is taken by the
+# inspector. Find the expected value of the number of good components in this
+# sample.
+
+###Solution :
+
+# # Let X represent the number of good components in the sample. 
+# The probability distribution of X is
+# f(x) = choose(4,x)*choose(3,3-x)/choose(7,3)
+
+x <- c(0,1,2,3)
+
+# Probability distribution vector
+f <- choose(4,x)*choose(3,3-x)/choose(7,3)
+exp_val = sum(x*f)
+#Answer
+round(exp_val,1)
